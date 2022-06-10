a61a21de03c373727a02694d9e07fd0b834c6a4a
perkun <g.dudzinski@protonmail.com>
2022-06-10 23:43:18 +0200

    refactor: initializer lists

--- src/Argument.h
+++ src/Argument.h
@@ -1,92 +1,82 @@
 #ifndef ARGUMENT_H_
 #define ARGUMENT_H_
 
-#include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-class Argument
+class OptionBase
 {
 public:
-	string short_name;
-	string long_name, description;
+    OptionBase(){};
+    OptionBase(string short_name, string long_name, string description)
+        : short_name(short_name), long_name(long_name), description(description)
+    {}
+    string short_name;
+    string long_name, description;
 };
 
-
-class Flag : public Argument
+class Flag : public OptionBase
 {
 public:
-	Flag(string short_name, string long_name, string description)
-	{
-		this->short_name = short_name;
-		this->long_name = long_name;
-		this->description = description;
-	}
-	bool status = false;
+    Flag(string short_name, string long_name, string description)
+        : OptionBase(short_name, long_name, description)
+    {}
+    bool status = false;
 };
 
-
-
-class Option : public Argument
+class Option : public OptionBase
 {
 public:
-	Option(string short_name, string long_name, string description,
-                        bool required, string default_value)
-	{
-		this->short_name = short_name;
-		this->long_name = long_name;
-		this->description = description;
-		this->required = required;
-		this->value = default_value;
-	}
-	string value;
-	bool required;
+    Option(string short_name, string long_name, string description,
+           bool required, string default_value)
+        : required(required),
+          value(default_value),
+          OptionBase(short_name, long_name, description)
+
+    {}
+    string value;
+    bool required;
 };
 
-
-
-class VectorOption : public Argument
+class VectorOption : public OptionBase
 {
 public:
-	VectorOption(string short_name, string long_name, string description,
+    VectorOption(string short_name, string long_name, string description,
                  int num_values, bool required)
-	{
-		this->short_name = short_name;
-		this->long_name = long_name;
-		this->description = description;
-		this->required = required;
-		this->num_values = num_values;
-	}
+        : num_values(num_values),
+          required(required),
+          OptionBase(short_name, long_name, description)
+    {}
 
-	int num_values;
-	vector<string> value_vec;
-	bool required;
+    int num_values;
+    vector<string> value_vec;
+    bool required;
 };
 
-class Positional : public Argument
+class Positional : public OptionBase
 {
 public:
-	Positional(string value, int position)
-	{
-		this->value = value;
-		this->position = position;
-	}
+    Positional(string value, int position)
+    {
+        this->value = value;
+        this->position = position;
+    }
 
-	string value;
-	int position;
-	bool required;
+    string value;
+    int position;
+    bool required;
 };
 
-class PositionalList : public Argument
+class PositionalList : public OptionBase
 {
 public:
-	int start_pos = 0;
-	bool required = false;
+    PositionalList() {}
+    int start_pos = 0;
+    bool required = false;
 
 private:
-
 };
-
 
 #endif /* ARGUMENT_H_ */
