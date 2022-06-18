d6c08536bec28cc4d93c05116bae21fb1101554e
perkun <g.dudzinski@protonmail.com>
2022-06-18 03:09:45 +0200

    Refactor

--- src/Args.cpp
+++ src/Args.cpp
@@ -1,65 +1,55 @@
 #include "Args.h"
 
+#include <algorithm>
+
 bool Args::operator[](std::string name)
 {
     for (Flag &f : flags)
     {
-        if (f.long_name == name) return f.status;
-        if (f.short_name != "")
-            if (f.short_name == name) return f.status;
+        if (f.long_name == name or
+            (not f.short_name.empty() and f.short_name == name))
+        {
+            return f.status;
+        }
     }
 
-    for (Option &opt : options)
-    {
-        if (opt.long_name == name && opt.value != "") return true;
-        if (opt.short_name != "")
-            if (opt.short_name == name && opt.value != "") return true;
-    }
-
-    for (VectorOption &opt : vec_options)
-    {
-        if (opt.long_name == name && opt.value_vec.size() != 0) return true;
-        if (opt.short_name != "")
-            if (opt.short_name == name && opt.value_vec.size() != 0)
-                return true;
-    }
-
-    for (Positional &p : positionals)
-    {
-        if (p.long_name == name) return true;
-    }
-
-    return false;
+    return is_option_defined(name) or is_vec_option_defined(name) or
+           is_positional_defined(name);
 }
 
 bool Args::is_defined(std::string name)
 {
-    for (Flag &f : flags)
-    {
-        if (f.long_name == name) return true;
-        if (f.short_name != "")
-            if (f.short_name == name) return true;
-    }
+    return is_flag_defined(name) or is_option_defined(name) or
+           is_vec_option_defined(name) or is_positional_defined(name);
+}
 
-    for (Option &opt : options)
-    {
-        if (opt.long_name == name && opt.value != "") return true;
-        if (opt.short_name != "")
-            if (opt.short_name == name && opt.value != "") return true;
-    }
+bool Args::is_flag_defined(std::string name)
+{
+    return std::any_of(flags.begin(), flags.end(), [&name](Flag &f) {
+        return (f.long_name == name) or
+               (not f.short_name.empty() and f.short_name == name);
+    });
+}
 
-    for (VectorOption &opt : vec_options)
-    {
-        if (opt.long_name == name && opt.value_vec.size() != 0) return true;
-        if (opt.short_name != "")
-            if (opt.short_name == name && opt.value_vec.size() != 0)
-                return true;
-    }
+bool Args::is_option_defined(std::string name)
+{
+    return std::any_of(options.begin(), options.end(), [&name](Option &opt) {
+        return (opt.long_name == name or
+                (not opt.short_name.empty() and opt.short_name == name));
+    });
+}
 
-    for (Positional &p : positionals)
-    {
-        if (p.long_name == name) return true;
-    }
+bool Args::is_vec_option_defined(std::string name)
+{
+    return std::any_of(
+        vec_options.begin(), vec_options.end(), [&name](VectorOption &opt) {
+            return (opt.long_name == name or
+                    (not opt.short_name.empty() and opt.short_name == name));
+        });
+}
 
-    return false;
+bool Args::is_positional_defined(std::string name)
+{
+    return std::any_of(positionals.begin(), positionals.end(),
+                       [&name](auto pos) { return pos.long_name == name; });
 }
--- src/Args.h
+++ src/Args.h
@@ -19,84 +19,21 @@ public:
     bool operator[](std::string name);  // i flag with true status, or has value
 
     template <typename T>
-    T get_value(std::string name)
-    {
-        if (!this->operator[](name))
-        {
-            print_error(ErrorMessages::option_not_given(name));
-            return T();
-        }
-
-        std::string value;
-
-        for (Option &opt : options)
-        {
-            if (opt.long_name == name && opt.value != "") value = opt.value;
-            if (opt.short_name != "")
-                if (opt.short_name == name && opt.value != "")
-                    value = opt.value;
-        }
-        return utils::convert_value<T>(value);
-    }
+    T get_value(std::string name);
 
     template <typename T>
-    std::vector<T> get_vec_values(std::string name)
-    {
-        if (!this->operator[](name))
-        {
-            print_error(ErrorMessages::option_not_given(name));
-            return std::vector<T>();
-        }
-
-        std::vector<T> return_values;
-        for (VectorOption &opt : vec_options)
-        {
-            if ((opt.long_name == name && opt.value_vec.size() != 0) or
-                ((opt.short_name != "") and
-                 (opt.short_name == name && opt.value_vec.size() != 0)))
-            {
-                for (std::string value : opt.value_vec)
-                {
-                    return_values.push_back(utils::convert_value<T>(value));
-                }
-            }
-        }
-        return return_values;
-    }
+    std::vector<T> get_vec_values(std::string name);
 
     template <typename T>
-    T get_positional(int position)
-    {
-        return utils::convert_value<T>(positionals.at(position).value);
-    }
+    T get_positional(int position);
 
     template <typename T>
-    T get_positional(std::string name)
-    {
-        for (Positional &p : positionals)
-        {
-            if (p.long_name == name)
-            {
-                return utils::convert_value<T>(p.value);
-            }
-        }
-
-        print_error(ErrorMessages::positional_not_given(name));
-        return T();
-    }
+    T get_positional(std::string name);
 
     template <typename T>
-    std::vector<T> get_all_positionals(int start_pos = 0)
-    {
-        std::vector<T> return_values;
-        for (int i = start_pos; i < positionals.size(); i++)
-        {
-            return_values.push_back(get_positional<T>(i));
-        }
-        return return_values;
-    }
+    std::vector<T> get_all_positionals(int start_pos = 0);
 
-    int num_positionals() { return positionals.size(); }
+    unsigned long num_positionals() { return positionals.size(); }
 
 private:
     bool is_defined(std::string name);
@@ -104,4 +41,95 @@ private:
     std::vector<Option> options;
     std::vector<VectorOption> vec_options;
     std::vector<Positional> positionals;
+
+//     Flag find_flag(std::string name);
+    bool is_flag_defined(std::string name);
+    bool is_option_defined(std::string name);
+    bool is_vec_option_defined(std::string name);
+    bool is_positional_defined(std::string name);
 };
+
+template <typename T>
+T Args::get_value(std::string name)
+{
+    if (!this->operator[](name))
+    {
+        print_error(ErrorMessages::option_not_given(name));
+        return {};
+    }
+
+    std::string value;
+
+    for (Option &opt : options)
+    {
+        if (opt.long_name == name && not opt.value.empty())
+        {
+            value = opt.value;
+        }
+        if (not opt.short_name.empty())
+        {
+            if (opt.short_name == name && not opt.value.empty())
+            {
+                value = opt.value;
+            }
+        }
+    }
+    return utils::convert_value<T>(value);
+}
+
+template <typename T>
+std::vector<T> Args::get_vec_values(std::string name)
+{
+    if (not this->operator[](name))
+    {
+        print_error(ErrorMessages::option_not_given(name));
+        return {};
+    }
+
+    std::vector<T> return_values;
+    for (VectorOption &opt : vec_options)
+    {
+        if ((opt.long_name == name && not opt.value_vec.empty()) or
+            ((not opt.short_name.empty()) and
+             (opt.short_name == name && not opt.value_vec.empty())))
+        {
+            for (std::string value : opt.value_vec)
+            {
+                return_values.push_back(utils::convert_value<T>(value));
+            }
+        }
+    }
+    return return_values;
+}
+
+template <typename T>
+T Args::get_positional(int position)
+{
+    return utils::convert_value<T>(positionals.at(position).value);
+}
+
+template <typename T>
+T Args::get_positional(std::string name)
+{
+    for (Positional &p : positionals)
+    {
+        if (p.long_name == name)
+        {
+            return utils::convert_value<T>(p.value);
+        }
+    }
+
+    print_error(ErrorMessages::positional_not_given(name));
+    return T();
+}
+
+template <typename T>
+std::vector<T> Args::get_all_positionals(int start_pos)
+{
+    std::vector<T> return_values;
+    for (int i = start_pos; i < positionals.size(); i++)
+    {
+        return_values.push_back(get_positional<T>(i));
+    }
+    return return_values;
+}
